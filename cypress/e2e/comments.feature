2f734d5c61c85f330f1a481feb266f1cd686517b
Mateo Castano <m.castano1@uniandes.edu.co>
2024-11-16 17:25:46 -0500

    :white_check_mark: Adding comments

--- cypress/e2e/comments.feature
+++ cypress/e2e/comments.feature
@@ -1,24 +1,10 @@
-Feature: Administración de Comentarios
+Feature: Administración de comentarios
 
 Scenario: Ver la lista de comentarios en la sección de administrador
   Given el administrador esta en la pagina de inicio de sesion
   When el administrador ingresa un correo "mateo.castano1@yahoo.com" y una contraseña "1qa2w3ed$%"
   And hace clic en el botón de inicio de sesión
-  And el usuario está en la página de comentarios
-  Then el usuario debería ver la lista de comentarios
-
-Scenario: Aprobar un comentario pendiente en la sección de administrador
-  Given el administrador esta en la pagina de inicio de sesion
-  When el administrador ingresa un correo "mateo.castano1@yahoo.com" y una contraseña "1qa2w3ed$%"
-  And hace clic en el botón de inicio de sesión
-  And el usuario está en la página de comentarios
-  When el usuario aprueba un comentario pendiente
-  Then el comentario debería estar marcado como aprobado
-
-Scenario: Eliminar un comentario en la sección de administrador
-  Given el administrador esta en la pagina de inicio de sesion
-  When el administrador ingresa un correo "mateo.castano1@yahoo.com" y una contraseña "1qa2w3ed$%"
-  And hace clic en el botón de inicio de sesión
-  And el usuario está en la página de comentarios
-  When el usuario elimina un comentario
-  Then el comentario no debería aparecer en la lista de comentarios
+  Then el usuario hace click en settings
+  Then el usuario hace click en editar settings
+  Then se le da permiso a todos los miembros para comentario
+  Then se guarda la configuracion de los comentarios
--- cypress/support/step_definitions/comments.ts
+++ cypress/support/step_definitions/comments.ts
@@ -1,33 +1,25 @@
 import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
 
-Given("el administrador ha iniciado sesión", () => {
-  cy.visit("/ghost/#/signin");
-  cy.get('input[name="identification"]').type("admin@ejemplo.com");
-  cy.get('input[name="password"]').type("admin123");
-  cy.get("button.login").click();
-  cy.url().should("include", "/ghost/#/site");
+Given("el usuario hace click en settings", () => {
+  cy.wait(1000);
+  cy.get("[data-test-nav='settings']").click();
 });
 
-Given("el administrador está en la página de comentarios", () => {
-  cy.visit("/ghost/#/comments");
+Then("el usuario hace click en editar settings", () => {
+  cy.wait(1000);
+  cy.get("#members").click();
 });
 
-Then("el administrador debería ver la lista de comentarios", () => {
-  cy.get(".comments-list").should("be.visible");
+Then("se le da permiso a todos los miembros para comentario", () => {
+  cy.wait(1000);
+  cy.get("[data-testid='access']").find('button:contains("Edit")').click();
 });
 
-When("el administrador aprueba un comentario pendiente", () => {
-  cy.get(".comment-pending").first().find(".approve-button").click();
-});
-
-Then("el comentario debería estar marcado como aprobado", () => {
-  cy.get(".comment-approved").should("have.length.greaterThan", 0);
-});
-
-When("el administrador elimina un comentario", () => {
-  cy.get(".comment").first().find(".delete-button").click();
-});
-
-Then("el comentario no debería aparecer en la lista de comentarios", () => {
-  cy.get(".comment").should("not.exist");
+Then("se guarda la configuracion de los comentarios", () => {
+  cy.wait(1000);
+  cy.get("[data-testid='commenting-select']").click();
+  cy.wait(1000);
+  cy.contains("All members").click();
+  cy.wait(1000);
+  cy.contains("Save").click();
 });
