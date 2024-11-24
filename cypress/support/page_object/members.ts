82189294a7396193addac30ab8241c40e33292b9
mateo <m.castano1@uniandes.edu.co>
2024-11-23 23:18:30 -0500

    ✅ First APD done

--- cypress/support/page_object/members.ts
+++ cypress/support/page_object/members.ts
@@ -1,18 +1,23 @@
 class Members {
   public visitMembersPage(url: string): void {
-    cy.visit(url);
-    cy.wait(100);
+    cy.get('[data-test-nav="members"]').click()
   }
 
   public clickNewMemberButton(): void {
     cy.get("[data-test-new-member-button='true']").click();
-    cy.wait(100);
   }
 
   public fillNewMemberForm(data: any): void {
     cy.get("[data-test-input='member-name']").type(data[0].name);
-    cy.wait(100);
     cy.get("[data-test-input='member-email']").type(data[0].email);
+  }
+
+  public clickSaveMember(): void {
+    cy.get('[data-test-button="save"]').click();
+  }
+
+  public showMemberCreated(): void {
+    cy.get('[data-test-table="members"]').should('be.visible');
   }
 }
 
--- cypress/support/step_definitions/rc/a-priori-data.ts
+++ cypress/support/step_definitions/rc/a-priori-data.ts
@@ -7,6 +7,14 @@ beforeEach(function () {
   });
 });
 
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // Si el error es el AbortError, devolver false para evitar que Cypress falle la prueba
+  if (err.name === 'AbortError') {
+    return false;
+  }
+  // Devuelve true o no devuelve nada para permitir que otros errores no capturados hagan que la prueba falle
+});
+
 When("el administrador ingresa a la pagina de miembros", function () {
   Members.visitMembersPage(this.data.membersUrl);
 });
@@ -17,4 +25,12 @@ Then("el administrador hace click en nuevo miembro", function () {
 
 Then("el administrador ingresa los datos del nuevo miembro", function () {
   Members.fillNewMemberForm(this.data.newMember);
+});
+
+Then("el administrador hace click en el botón de guardar", function () {
+  Members.clickSaveMember();
+})
+
+Then('el administrador deberia ver el nuevo miembro en la lista de miembros', function(){
+  Members.showMemberCreated();
 });
